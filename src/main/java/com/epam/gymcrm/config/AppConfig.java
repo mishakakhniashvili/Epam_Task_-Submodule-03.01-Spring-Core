c2c76c101a7db6d94a709a3aa6569a2555bb2a8d
Mikheili Kakhniashvili <mikheili_kakhniashvili@epam.com>
2026-05-10 03:28:37 +0400

    add domain model classes

--- src/main/java/com/epam/gymcrm/config/AppConfig.java
+++ src/main/java/com/epam/gymcrm/config/AppConfig.java
@@ -1,4 +1,5 @@
 package com.epam.gymcrm.config;
 
 public class AppConfig {
+
 }
--- src/main/java/com/epam/gymcrm/model/TrainingType.java
+++ src/main/java/com/epam/gymcrm/model/TrainingType.java
@@ -1,4 +1,21 @@
 package com.epam.gymcrm.model;
 
 public class TrainingType {
+
+    private String trainingTypeName;
+
+    public TrainingType() {
+    }
+
+    public TrainingType(String trainingTypeName) {
+        this.trainingTypeName = trainingTypeName;
+    }
+
+    public String getTrainingTypeName() {
+        return trainingTypeName;
+    }
+
+    public void setTrainingTypeName(String trainingTypeName) {
+        this.trainingTypeName = trainingTypeName;
+    }
 }

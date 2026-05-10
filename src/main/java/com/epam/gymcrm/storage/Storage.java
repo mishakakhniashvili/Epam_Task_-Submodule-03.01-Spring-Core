6ed6d7130f508f0a0b95af13ea13e880624d55ad
Mikheili Kakhniashvili <mikheili_kakhniashvili@epam.com>
2026-05-10 04:33:17 +0400

    add in memory storage

--- src/main/java/com/epam/gymcrm/storage/Storage.java
+++ src/main/java/com/epam/gymcrm/storage/Storage.java
@@ -1,4 +1,33 @@
 package com.epam.gymcrm.storage;
 
+import com.epam.gymcrm.model.Trainee;
+import com.epam.gymcrm.model.Trainer;
+import com.epam.gymcrm.model.Training;
+import com.epam.gymcrm.model.TrainingType;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Storage {
+
+    private final Map<Long, Trainee> trainees = new HashMap<>();
+    private final Map<Long, Trainer> trainers = new HashMap<>();
+    private final Map<Long, Training> trainings = new HashMap<>();
+    private final Map<String, TrainingType> trainingTypes = new HashMap<>();
+
+    public Map<Long, Trainee> getTrainees() {
+        return trainees;
+    }
+
+    public Map<Long, Trainer> getTrainers() {
+        return trainers;
+    }
+
+    public Map<Long, Training> getTrainings() {
+        return trainings;
+    }
+
+    public Map<String, TrainingType> getTrainingTypes() {
+        return trainingTypes;
+    }
 }
